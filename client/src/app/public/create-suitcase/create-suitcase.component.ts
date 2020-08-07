3cda4cf14304f57cdf3bcd7e7b5a92f4628e4bca
Alejandro Manso <alejandro.mansogonzalez@kbc.be>
2020-08-07 18:03:59 +0200

    add next screen

---
+++ client/src/app/public/create-suitcase/create-suitcase.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-create-suitcase',
+  templateUrl: './create-suitcase.component.html',
+  styleUrls: ['./create-suitcase.component.scss']
+})
+export class CreateSuitcaseComponent implements OnInit{
+
+  constructor(
+  ) { }
+
+  ngOnInit() {
+  }
+}
