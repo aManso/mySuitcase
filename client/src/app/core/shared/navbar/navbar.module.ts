72868556168db19d1c0e938088a74aa16600af85
Manso <450622@cognizant.com>
2022-08-23 16:13:37 +0200

    fix icons in menu for larger screens

--- client/src/app/core/shared/navbar/navbar.module.ts
+++ client/src/app/core/shared/navbar/navbar.module.ts
@@ -2,12 +2,17 @@ import { NgModule } from '@angular/core';
 import { NavBarComponent } from './containers/navbar.component';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { MatIconModule } from '@angular/material/icon';
 
 @NgModule({
 
   imports: [
     RouterModule, // needed for routerLink in view
     CommonModule, // needed for async in view
+    // Angular Material
+    MatTooltipModule,
+    MatIconModule,
   ],
   declarations: [
     NavBarComponent,
