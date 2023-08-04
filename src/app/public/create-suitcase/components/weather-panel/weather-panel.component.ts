f99c02deda5a566db0da1a71adbe8000726ec9a0
Alex <alexmanso27@hotmail.com>
2023-08-04 11:35:56 +0200

    show mock weather data for dev purposes

--- src/app/public/create-suitcase/components/weather-panel/weather-panel.component.ts
+++ src/app/public/create-suitcase/components/weather-panel/weather-panel.component.ts
@@ -30,8 +30,9 @@ export class WeatherPanelComponent implements OnInit {
       this._parseData(response);
     }, (error) => {
       console.log(error);
-      // TODO just for testing;
-      const mockResponse = {lat:36.72,lon:-4.42,timezone:'Europe/Madrid',timezone_offset:7200,daily:[
+      if (!environment.production) {
+        // TODO just for testing;
+        const mockResponse = {lat:36.72,lon:-4.42,timezone:'Europe/Madrid',timezone_offset:7200,daily:[
         {dt:1599393600,sunrise:1599371589,sunset:1599417546,temp:{day:299.24,min:295.58,max:299.24,night:295.58,eve:298.54,morn:299.24},feels_like:{day:300.78,night:297.24,eve:300.35,morn:300.78},pressure:1015,humidity:69,dew_point:293.11,wind_speed:3.04,wind_deg:146,weather:[{id:801,main:'Clouds',description:'few clouds',icon:'02d'}],clouds:20,pop:0,uvi:8.02},
           {dt:1599480000,sunrise:1599458036,sunset:1599503857,temp:{day:300.85,min:294.59,max:300.85,night:295.29,eve:298.14,morn:294.59},feels_like:{day:300.92,night:296.78,eve:299.2,morn:296.39},pressure:1015,humidity:51,dew_point:289.8,wind_speed:3.09,wind_deg:161,weather:[{id:800,main:'Clear',description:'clear sky',icon:'01d'}],clouds:0,pop:0,uvi:7.61},
           {dt:1599566400,sunrise:1599544484,sunset:1599590168,temp:{day:298.26,min:295.2,max:298.8,night:295.2,eve:297,morn:295.74},feels_like:{day:296.38,night:295.64,eve:296.93,morn:297.69},pressure:1018,humidity:61,dew_point:290.43,wind_speed:6.11,wind_deg:127,weather:[{id:500,main:'Rain',description:'light rain',icon:'10d'}],clouds:71,pop:0.46,rain:1.07,uvi:7.31},
@@ -41,7 +42,8 @@ export class WeatherPanelComponent implements OnInit {
           // {dt:1599912000,sunrise:1599890273,sunset:1599935406,temp:{day:301.46,min:294.71,max:301.46,night:297.07,eve:299.45,morn:294.71},feels_like:{day:300.65,night:298.3,eve:300.19,morn:295.63},pressure:1014,humidity:41,dew_point:287.02,wind_speed:2.86,wind_deg:172,weather:[{id:804,main:'Thunderstorm',description:'thunderstorm',icon:'04d'}],clouds:95,pop:0,uvi:7.32},
           {dt:1599998400,sunrise:1599976720,sunset:1600021715,temp:{day:300.87,min:296.87,max:301.18,night:297.67,eve:299.01,morn:296.87},feels_like:{day:300.73,night:298.94,eve:299.75,morn:297.34},pressure:1018,humidity:50,dew_point:289.75,wind_speed:3.23,wind_deg:160,weather:[{id:804,main:'Clouds',description:'overcast clouds',icon:'04d'}],clouds:100,pop:0.1,uvi:7.38}
           ]};
-      this._parseData(mockResponse);
+        this._parseData(mockResponse);
+      }
     })
   }
 
