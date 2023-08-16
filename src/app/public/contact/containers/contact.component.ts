347f6e343e12916440db31e43022fa3ef11d4bde
Alex <alexmanso27@hotmail.com>
2023-08-16 14:05:16 +0200

    fix style issues with forms

--- src/app/public/contact/containers/contact.component.ts
+++ src/app/public/contact/containers/contact.component.ts
@@ -9,6 +9,7 @@ import { ContactService } from '../contact.service';
 @Component({
   selector: 'app-home',
   templateUrl: './contact.component.html',
+  styleUrls: ['./contact.component.scss'],
 })
 export class ContactComponent implements OnInit {
   public contactForm: FormGroup;
