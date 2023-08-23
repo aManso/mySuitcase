fc017aa268639eb15fd9997943897f3d2c221820
Alex <alexmanso27@hotmail.com>
2023-08-23 09:42:38 +0200

    remove section from website and create a reusable component for future use

---
+++ src/app/core/shared/working-in-progress/working-in-progress.component.ts
@@ -0,0 +1,10 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'app-working-in-progress',
+  templateUrl: './working-in-progress.component.html',
+  standalone: true,
+})
+export class WorkingInProgressComponent {
+
+}
