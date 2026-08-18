db6e39c7540f315103bb928525c3d413187bb86e
Alejandro Manso <alexmanso27@hotmail.com>
2026-08-18 08:44:47 +0200

    set correct file path

--- src/app/core/services/pwa-installer.service.ts
+++ src/app/core/services/pwa-installer.service.ts
@@ -6,7 +6,7 @@ export class PWAInstallerService {
         let body = <HTMLDivElement> document.body;
         let script = document.createElement('script');
         script.innerHTML = 'Install APP';
-        script.src = '/pwa-installer.js';
+        script.src = './pwa-installer.js';
         script.async = true;
         script.defer = true;
         body.appendChild(script);
